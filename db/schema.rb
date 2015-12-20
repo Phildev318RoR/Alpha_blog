777668b9b2721c703d1db2942253e537ae78dbf3
Philip de Villiers <phildev318@gmail.com>
2015-12-20 00:12:38 +0000

    Create user table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215160314) do
+ActiveRecord::Schema.define(version: 20151219235940) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -151,6 +151,13 @@ ActiveRecord::Schema.define(version: 20151215160314) do
     t.string   "address3"
     t.boolean  "downloaded"
     t.integer  "parent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
