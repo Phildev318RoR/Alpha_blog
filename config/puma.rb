91ee4b1ec1933de68a56a879cba609716e3e39b8
Philip de Villiers <phildev318@gmail.com>
2016-01-07 10:31:28 +0000

    Update puma out1

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,42 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+#!/usr/bin/env puma
 
-preload_app!
+# app do |env|
+#   puts env
+#
+#   body = 'Hello, World!'
+#
+#   [200, { 'Content-Type' => 'text/plain', 'Content-Length' => body.length.to_s }, [body]]
+# end
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+environment 'production'
+daemonize false
 
-on_worker_boot do
-  # worker specific setup
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-             Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
-    ActiveRecord::Base.establish_connection(config)
-  end
-end
+pidfile 'tmp/pids/puma.pid'
+state_path 'tmp/pids/puma.state'
+
+# stdout_redirect 'log/puma.log', 'log/puma_err.log'
+
+# quiet
+threads 0, 16
+bind 'unix://tmp/sockets/puma.sock'
+
+# ssl_bind '127.0.0.1', '9292', { key: path_to_key, cert: path_to_cert }
+
+# on_restart do
+#   puts 'On restart...'
+# end
+
+# restart_command '/u/app/lolcat/bin/restart_puma'
+
+
+# === Cluster mode ===
+
+# workers 2
+
+# on_worker_boot do
+#   puts 'On worker boot...'
+# end
+
+# === Puma control rack application ===
+
+activate_control_app 'unix://tmp/sockets/pumactl.sock'
